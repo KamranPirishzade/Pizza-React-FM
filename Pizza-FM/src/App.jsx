05941aa33962595c3f4854b653b0c01889759337
Kamran <KamranPirishzade@gmail.com>
2025-08-05 03:28:35 -0700

    Implement useContext to handle cart

--- Pizza-FM/src/App.jsx
+++ Pizza-FM/src/App.jsx
@@ -1,14 +1,17 @@
-import Pizza from "./Pizza";
 import Order from "./Order";
 import PizzaOfTheDay from "./PizzaOfTheDay";
+import Header from "./Header";
+import { CartContext } from "./contexts";
+import { useState } from "react";
 
 const App = () => {
+  const cartHook = useState([]);
   return (
-    <div>
-      <h1 className="logo">Padre Gino's - Order Now</h1>
+    <CartContext.Provider value={cartHook}>
+      <Header />
       <Order />
       <PizzaOfTheDay />
-    </div>
+    </CartContext.Provider>
   );
 };
 
